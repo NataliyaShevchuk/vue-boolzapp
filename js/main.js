d675b40775e86f9edbda44521debea61fd62d6b6
NataliyaShevchuk <nata.shevchuk@icloud.com>
2022-11-28 18:22:50 +0100

    milestone 3 fatta

--- js/main.js
+++ js/main.js
@@ -10,13 +10,31 @@ createApp ({
     return{
             usersList: importedUsersList,
             selectedUser: null, //rappresenta l'utente che ho cliccato
+            textInput: " ",
+            
         }
     },
     methods: {
-
+        
+        onNewMessageTextInput(){
+            this.selectedUser.messages.push({
+                hour: luxon.DateTime.now().toFormat('dd/MM HH:mm'),
+                message: this.textInput,
+                status: 'sent'
+            })
+            this.textInput = " ";
+            setTimeout(this.okMessageFromUser, 1000);
+        },
+        okMessageFromUser (){
+                this.selectedUser.messages.push({
+                    status: 'received',
+                    hour: luxon.DateTime.now().toFormat('dd/MM HH:mm'),
+                    message: 'Okay',
+                })
+        }
     },
     beforeMount (){ // in questo modo ancora prima che lui stampi l'html io li assegno un valore alla variabile e 
         // e poi quando lui stamperà l'html, la variabile avrà già un valore
         this.selectedUser = this.usersList[0];  
-    }
+    },
 }).mount("#app")
