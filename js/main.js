2bcde37383217caac4ed346a9bf742972879d011
NataliyaShevchuk <nata.shevchuk@icloud.com>
2022-11-29 21:45:09 +0100

    milestone 5 fatta, problemi con la 4

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+import usersList from "./usersList.js";
 import importedUsersList from "./usersList.js";
 
 //dall'oggetto globale Vue, estraggo in una variabile la funzione CreateApp
@@ -10,12 +11,13 @@ createApp ({
     return{
             usersList: importedUsersList,
             selectedUser: null, //rappresenta l'utente che ho cliccato
-            textInput: " ",
-            
+            textInput: null,
+            contactInput: " ",
+            lastSeenDate: luxon.DateTime.now().toFormat('dd/MM'),
+            lastSeenHour: luxon.DateTime.now().toFormat('HH:mm'),
         }
     },
     methods: {
-        
         onNewMessageTextInput(){
             this.selectedUser.messages.push({
                 hour: luxon.DateTime.now().toFormat('dd/MM HH:mm'),
@@ -31,10 +33,23 @@ createApp ({
                     hour: luxon.DateTime.now().toFormat('dd/MM HH:mm'),
                     message: 'Okay',
                 })
+        },
+        lookingForAContact (){
+            return this.usersList.filter(user =>{
+                return this.user.name.toLowerCase().includes(this.contactInput.toLowerCase());
+                
+            });
+        },
+        clickInfoMessage(index){
+            alert(`Data invio: ${this.selectedUser.messages[index].date}`);
+        },
+        clickDeleteMessage (index){
+            this.selectedUser.messages.splice(index, 1);
         }
-    },
-    beforeMount (){ // in questo modo ancora prima che lui stampi l'html io li assegno un valore alla variabile e 
-        // e poi quando lui stamperà l'html, la variabile avrà già un valore
-        this.selectedUser = this.usersList[0];  
-    },
+
+        },
+        beforeMount (){ // in questo modo ancora prima che lui stampi l'html io li assegno un valore alla variabile e 
+            // e poi quando lui stamperà l'html, la variabile avrà già un valore
+            this.selectedUser = this.usersList[0];  
+        }
 }).mount("#app")
